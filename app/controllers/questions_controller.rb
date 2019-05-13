885263c70da5cde2826b40762136f10de53aa145
Robert Simon <robertsimonmedia@gmail.com>
2019-05-13 14:32:47 -0400

    added css styling and completed all actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,17 @@
 # app/controllers/questions_controller.rb
 class QuestionsController < ApplicationController
-  def answer
-  end
   def ask
+  end
+
+  def answer
+    # @answer = ''
+    @question = params['question']
+    if @question == 'I am going to work'
+      @answer = 'Great'
+    elsif @question.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
   end
 end
